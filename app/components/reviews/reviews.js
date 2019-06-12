ea256805066c8e4bf4e4704547ed588ca6577d15
kkuleshov92 <nstkuleshov1@gmail.com>
2019-06-12 13:22:03 +0300

    reviews

--- app/components/reviews/reviews.js
+++ app/components/reviews/reviews.js
@@ -2,5 +2,32 @@ $('.reviews__list').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     dots: false,
-    variableWidth: true
+    speed: 500,
+    variableWidth: true,
+    prevArrow: $('.reviews__arrow_left'),
+    nextArrow: $('.reviews__arrow_right'),
+    centerMode: true,
+    responsive: [
+        {
+            breakpoint: 1023,
+            settings: {
+                slidesToShow: 2,
+                centerMode: false
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
+});
+
+$('.icon-more').click(function(){
+   $(this).siblings('.reviews__text').toggleClass('active');
+});
+
+$('.reviews__list').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    $('.reviews__text').removeClass('active');
 });
